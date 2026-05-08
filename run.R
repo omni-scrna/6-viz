0c84316d528cfd053a8301b93000881503dd87e2
Mark Robinson <mark.robinson@mls.uzh.ch>
2026-05-08 09:11:55 +0200

    get the basic parts there

--- run.R
+++ run.R
@@ -1,6 +1,8 @@
 #!/usr/bin/env Rscript
 
-library(argparse)
+suppressPackageStartupMessages({
+  library(argparse)
+})
 
 # Source main functions
 source("src/main.R")
@@ -14,16 +16,50 @@ parser$add_argument("--output_dir", dest="output_dir", type="character", require
 parser$add_argument("--name", dest="name", type="character", required=TRUE,
                    help="Module name/identifier")
 # Stage-specific inputs
-parser$add_argument("--pca", dest="pca",
-                   type="character", nargs="+", required=TRUE,
-                   help="Input: pca")
+parser$add_argument("--pcas.tsv", dest="pcas",
+                   type="character", required=FALSE,
+                   help="Input: PCs TSV file")
+parser$add_argument("--flavour", dest="flavour",
+                   type="character", required=FALSE,
+                   help="Input: flavour on non-linear embedding")
+#parser$add_argument("--perplexity", dest="perplexity",
+#                   type="integer", required=FALSE,
+#                   help="Input: perplexity value")
 
 args <- parser$parse_args()
 
 cat("Output directory:", args$output_dir, "\n")
 cat("Module name:", args$name, "\n")
-cat("pca:", args$pca, "\n")
+cat("pca:", args$pcas, "\n")
+cat("flavour:", args$flavour, "\n")
 
-# TODO: Implement your module logic
-# Process the data using main function
-process_data(args)
+
+# possible scrapper_UMAP params to expose
+#> scrapper:::run_umap
+#function (nnidx, nndist, ndim, min_dist, seed, num_epochs, num_threads, 
+#    parallel_optimization) 
+#{
+#    .Call("_scrapper_run_umap", PACKAGE = "scrapper", nnidx, 
+#        nndist, ndim, min_dist, seed, num_epochs, num_threads, 
+#        parallel_optimization)
+#}
+
+
+if (args$flavour == "scrapper_UMAP") {
+  require(scrapper)
+  require(data.table)
+  pca <- fread(args$pcas, header = TRUE)
+  embed <- runUmap(pca) # maybe add params
+} else if (args$flavour == "XXX") {
+  # do something here
+} else {
+  errorCondition("incorrect 'flavour' specified")
+}
+
+out <- file.path(args$output_dir, paste0(args$name, "_lowdviz.tsv"))
+cat("output_file:", out, "\n")
+fwrite(data.frame(cell_id = rownames(embed), embed), out, 
+       sep = "\t", quote = FALSE, row.names = FALSE)
+file.info(out)[,c("size", "ctime")]
+
+
